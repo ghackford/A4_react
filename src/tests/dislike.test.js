f5b2baa143869d9964741a978183e3d2855a3c9b
ghackford <hackford.j@northeastern.edu>
2022-03-29 00:35:52 -0400

    fleshing out test for disliking a tuit

--- src/tests/dislike.test.js
+++ src/tests/dislike.test.js
@@ -1,9 +1,9 @@
 import {
     findAllTuitsDislikedByUser,
     findAllUsersThatDislikedTuit,
-    userDislikesTuit
+    userDislikesTuit, deleteDislike
 } from "../services/dislikes-service";
-import { createTuit } from "../services/tuits-service";
+import { createTuit, deleteTuit, findTuitById } from "../services/tuits-service";
 import {createUser, deleteUsersByUsername} from "../services/users-service";
 
 describe('userDislikesTuit', () => {
@@ -18,6 +18,7 @@ describe('userDislikesTuit', () => {
         email: 'dislike@testing.com'
     };
 
+
     beforeAll(() => {
         deleteUsersByUsername(normalUser.username);
         return deleteUsersByUsername(dislikeUser.username);
@@ -28,12 +29,24 @@ describe('userDislikesTuit', () => {
         return deleteUsersByUsername(dislikeUser.username);
     })
     test('can dislike a tuit', async () => {
-        //stuff
+        //create necessary users and tuits
         const normal = await createUser(normalUser);
         const tuitContents = {tuit: "this is my normal tuit"};
         const tuitToDislike = await createTuit(normal._id, tuitContents);
-
         const dislike = await createUser(dislikeUser);
-        
+
+        //query server for updated data objects
+        const disliking = await userDislikesTuit(dislike._id, tuitToDislike._id);
+        const dislikedTuit = await findTuitById(tuitToDislike._id);
+        const dislikeObjectWrapped = await findAllUsersThatDislikedTuit(dislikedTuit._id);
+        const dislikeObject = dislikeObjectWrapped[0];
+
+        expect(dislikedTuit.stats['dislikes']).toEqual(1);
+        expect(dislikeObject.tuit).toEqual(dislikedTuit._id);
+        expect(dislikeObject.dislikedBy._id).toEqual(dislike._id);
+
+        //clean up
+        deleteTuit(dislikedTuit._id);
+        deleteDislike(dislikeObject._id);
     })
 })
