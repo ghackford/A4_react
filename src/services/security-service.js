27f1e7f237b68b374df280226aa8d50a7c7306ae
ghackford <hackford.j@northeastern.edu>
2022-03-31 00:34:24 -0400

    removing unnecessary debug code

--- src/services/security-service.js
+++ src/services/security-service.js
@@ -9,18 +9,6 @@ const api = axios.create({
     withCredentials: true
 });
 
-api.interceptors.request.use(request => {
-    console.log('Starting Request')
-    console.log(request)
-    return request
-})
-
-api.interceptors.response.use(response => {
-    console.log('Received response')
-    console.log(response)
-    return response
-})
-
 export const register = (user) =>
     api.post(`${SECURITY_API}/register`, user)
         .then(response => response.data);
@@ -28,17 +16,6 @@ export const register = (user) =>
  export const login = (user) =>
      api.post(`${SECURITY_API}/login`, user)
          .then(response => response.data);
-
-/*export const login = (user) => {
-    console.log("login function");
-    console.log(user);
-    return api.post(`${SECURITY_API}/login`, user)
-        .then(response => {
-            console.log("login response from server:");
-            console.log(response);
-            return response.data;
-        });
-}*/
 
 export const logout = (user) =>
     api.post(`${SECURITY_API}/logout`, user)
